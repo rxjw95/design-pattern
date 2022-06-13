4e6e6b46a93ec7b1e880fb253dcbedcc0ecef77d
Jangwook Ryu <rxjw95@gmail.com>
2022-06-13 19:53:12 +0900

    [singleton]
    - thread-safety early initialized singleton code

--- src/main/java/creation/singleton/SettingsSingleton.java
+++ src/main/java/creation/singleton/SettingsSingleton.java
@@ -2,15 +2,11 @@ package creation.singleton;
 
 public class SettingsSingleton {
 
-    private static SettingsSingleton singleton;
+    private static final SettingsSingleton singleton = new SettingsSingleton();
 
     private SettingsSingleton() {}
 
     public static synchronized SettingsSingleton getInstance() {
-        if(singleton == null) {
-            singleton = new SettingsSingleton();
-        }
-
         return singleton;
     }
 }

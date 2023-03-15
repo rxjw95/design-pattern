6be16281aa456607d182e11c9eeef154103d98ed
Jangwook Ryu <rxjw95@gmail.com>
2023-03-16 00:09:19 +0900

    SRP - invalid example

---
+++ src/main/java/solid/srp/invalid/AClient.java
@@ -0,0 +1,11 @@
+package solid.srp.invalid;
+
+import java.util.Collections;
+import java.util.List;
+
+public class AClient {
+
+    public void service(List<Student> group) {
+        Collections.sort(group);
+    }
+}
